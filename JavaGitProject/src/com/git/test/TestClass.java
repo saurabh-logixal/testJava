e84109fdb98ee77e879df14f54e85695dea15fb1
saurabh-logixal <45450149+saurabh-logixal@users.noreply.github.com>
2019-06-24 12:17:22 +0530

    Second change
    Added second change to the project

--- JavaGitProject/src/com/git/test/TestClass.java
+++ JavaGitProject/src/com/git/test/TestClass.java
@@ -3,6 +3,6 @@ package com.git.test;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("This is a change");
-		
+		System.out.println("This is a second change");	
 	}
 }
